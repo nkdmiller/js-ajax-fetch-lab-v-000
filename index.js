62d10bd55102e57b5d656641c0845bbfcff18b63
Nate Miller <natekmiller96@gmail.com>
2018-10-29 18:34:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function forkRepo() {
   fetch(url, {
   method: 'POST',
   headers: {
-      Authorization: `token ${token}`
+      Authorization: `token ${getToken()}`
     }
 }).then(res => res.json())
 .then(json => showResults(json))
