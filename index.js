cf0fd05c117aa106ebb668419229f16689864786
Nate Miller <natekmiller96@gmail.com>
2018-10-29 18:03:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+ return '3487288b48b2d6feddbd2c9d0bbf96c2ddd229ea';
+  
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
-}
+  const url = "https://api.github.com/repos"
+  fetch(url, {
+  method: 'POST',
+  headers: {
+      Authorization: `token ${token}`
+    }
+}).then(res => res.json())
+.then(json => showResults(json))
+  
+  }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
